ca5db945f3f9f63ddf3cc8e4b5cefdc6240b5546
Alejandro Paez <alejandropt@utexas.edu>
2018-11-08 19:49:56 -0600

    Commit #1 HTML in loginRegister. All nave working

---
+++ js/indexQuery.js
@@ -0,0 +1,24 @@
+$('#menu > li').on('click', function(event){
+
+	//$('.selectedNavElement').removeClass('selectedNavElement');
+
+	let currentElement = $(this);
+	let sectionName = currentElement.attr('class');
+
+  if(sectionName == "home")
+    $(location).attr("href", "./index.html");
+
+  if(sectionName == "vote")
+    $(location).attr("href", "./vote.html");
+
+  if(sectionName == "upload")
+    $(location).attr("href", "./upload.html");
+
+  if(sectionName == "loginRegister")
+    $(location).attr("href", "./loginRegister.html");
+
+  if(sectionName == "about")
+    $(location).attr("href", "./about.html");
+
+
+});
