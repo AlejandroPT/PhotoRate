47ea4826f0ba3afb671024c0f772c763e0126701
Alejandro Paez <alejandropt@utexas.edu>
2018-11-11 13:14:31 -0600

    Vote displays two random photos, their authors and tittles Still missing style, the actual voting process, and the option for categories, as well as uploading with categories

--- js/voteQuery.js
+++ js/voteQuery.js
@@ -22,3 +22,41 @@ $('#menu > li').on('click', function(event){
 
 
 });
+
+jsonToSend = {"action" : "PHOTOS"};
+$.ajax({
+  url : './data/app.php',
+  type : 'GET',
+  data : jsonToSend,
+  ContentType : "application/json",
+  dataType : 'json',
+  success : function(data){
+    var index1 = Math.floor(Math.random() * data.length);
+    var index2 = Math.floor(Math.random() * data.length);
+
+    let pht1 = data[index1];
+    let pht2 = data[index2];
+
+    let author1 = `${pht1['username']}`;
+    let author2 = `${pht2['username']}`;
+
+    let tittle1 = `${pht1['tittle']}`;
+    let tittle2 = `${pht2['tittle']}`;
+
+    let loc1 = `${pht1['img']}`;
+    let loc2 = `${pht2['img']}`;
+
+    let correctLoc1 = loc1.replace('..','.');
+    let correctLoc2 = loc2.replace('..','.');
+    let newHtml =
+      '<span class="author1">' + author1 + "</span>" +'<span class="author2">' + author2 + "</span>"
+      +'<br><span class="tittle1">'+ tittle1 + "</span>" +'<span class="tittle2">'+ tittle2 + "</span>"
+      + '<br><img src ="' + correctLoc1 + '" width=40% class="img1">' + '<img src ="' + correctLoc2 + '"width=40% class="img2">';
+    $('#photos').append(newHtml);
+    console.log(data);
+  },
+  error : function(errorMsg){
+    console.log("bye");
+    console.log(errorMsg);
+  }
+});
